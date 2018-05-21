2cb033a964dbf5629f1281d855a939e653d7bd90
twinster <gochitashvili.ilia@gmail.com>
2018-05-21 22:28:43 +0400

    added sound to notification

--- app/src/main/java/com/example/twinster/socialnetwork/MessagingService.java
+++ app/src/main/java/com/example/twinster/socialnetwork/MessagingService.java
@@ -2,6 +2,8 @@ package com.example.twinster.socialnetwork;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -17,10 +19,13 @@ public class MessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
+        Uri sound= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
         Notification.Builder builder = new Notification.Builder(MessagingService.this)
                 .setSmallIcon(R.mipmap.notification)
                 .setContentTitle("new Friend Request")
-                .setContentText("You`ve Received New Notification");
+                .setContentText("You`ve Received New Notification")
+                .setSound(sound);
 
         int notificationId = (int) System.currentTimeMillis();
 
