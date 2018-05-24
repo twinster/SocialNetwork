7b3c217244cebec66eed73d59d7bd421434a5418
Guga <gugamaxara25@gmail.com>
2018-05-24 23:54:06 +0400

    little bit changes in message_row

--- app/src/main/java/com/example/twinster/socialnetwork/MessagesAdapter.java
+++ app/src/main/java/com/example/twinster/socialnetwork/MessagesAdapter.java
@@ -132,7 +132,7 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.Messag
         }
 
         if (from_user.equals(current_user_id)) {
-            holder.messageText.setBackgroundColor(Color.BLUE);
+            holder.messageText.setBackgroundColor(R.drawable.sms_background);
             holder.messageText.setTextColor(Color.WHITE);
         }
         else{
